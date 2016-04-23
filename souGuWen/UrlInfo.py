56f52b1fa2ec69889081914e2a674335288fe4ee
opening2000 <yankj12@163.com>
2016-04-24 01:09:43 +0800

    UrlInfo module use DBConnection

--- souGuWen/UrlInfo.py
+++ souGuWen/UrlInfo.py
@@ -4,33 +4,15 @@
 
 
 
-import MySQLdb
-import ConfigParser
-import string, sys
-
-
-
-#从db.conf中读取配置参数
-cf = ConfigParser.ConfigParser()
- 
-cf.read("db.conf")
-
-db_host = cf.get("db", "db_host")
-db_port = cf.getint("db", "db_port")
-db_user = cf.get("db", "db_user")
-db_pass = cf.get("db", "db_pass")
-db_database = cf.get("db", "db_database")
-
-#conn = None
-#cur = None
-
+import time
+import DBConnectionUtil
 
 
 def findUrlInfoByUrl(url):
-    conn , cur = createConn()
+    conn , cur = DBConnectionUtil.createConn()
     cur.execute('select content from url_cache where url= %s ' , (url,))
     result = cur.fetchone()
-    closeConn(conn , cur)
+    DBConnectionUtil.closeConn(conn , cur)
     #print result is not None    #is not None 是非空
     #print len(result)
     if result and len(result) == 1:
@@ -40,10 +22,10 @@ def findUrlInfoByUrl(url):
 
 
 def findUrlInfoByUrlMd5(urlMd5):
-    conn , cur = createConn()
+    conn , cur = DBConnectionUtil.createConn()
     cur.execute('select content from url_cache where urlmd5= %s ' , (urlMd5,))
     result = cur.fetchone()
-    closeConn(conn , cur)
+    DBConnectionUtil.closeConn(conn , cur)
     #print result is not None    #is not None 是非空
     #print len(result)
     if result and len(result) == 1:
@@ -55,22 +37,8 @@ def findUrlInfoByUrlMd5(urlMd5):
 def insertUrlInfoByUrl(url , content):
     urlmd5 = getMd5Str(url)
     nowStr = time.strftime('%Y-%m-%d %H:%M:%S')
-    conn , cur = createConn()
+    conn , cur = DBConnectionUtil.createConn()
     cur.execute('insert into url_cache(url ,urlmd5 , content ,inserttime ,updatetime) values(%s,%s,%s,%s,%s)', (url , urlmd5 , content , nowStr , nowStr))
     conn.commit()
-    closeConn(conn , cur)
+    DBConnectionUtil.closeConn(conn , cur)
 
-
-#mysql
-def createConn():
-    conn = MySQLdb.connect(host = db_host , user = db_user , passwd = db_pass , port = db_port , charset='utf8')
-    cur = conn.cursor()
-    conn.select_db(db_database)
-    conn.autocommit(1) 
-    
-    return conn , cur
-
-def closeConn( conn , cur):
-    conn.commit()
-    cur.close()
-    conn.close()
