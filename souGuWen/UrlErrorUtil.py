bca61584bf1878e10a776ab2e21e25c6629e6342
opening2000 <yankj12@163.com>
2016-04-25 23:23:05 +0800

    use urlError class

--- souGuWen/UrlErrorUtil.py
+++ souGuWen/UrlErrorUtil.py
@@ -8,6 +8,31 @@ import time
 import DBConnectionUtil
 
 
+
+class UrlError:
+    
+    def __init__(self, urlMd5, url, rightUrlMd5, rightUrl, errorMessage, solveFlag, solution, insertTime, updateTime):
+        self.urlMd5 = urlMd5
+        self.url = url
+        self.rightUrlMd5 = rightUrlMd5
+        self.rightUrl = rightUrl
+        self.errorMessage = errorMessage
+        self.solveFlag = solveFlag
+        self.solution = solution
+        self.insertTime = insertTime
+        self.updateTime = updateTime
+
+
+#向url_error插入一条记录
+def insertUrlError(urlError):
+    nowStr = time.strftime('%Y-%m-%d %H:%M:%S')
+    conn , cur = DBConnectionUtil.createConn()
+    cur.execute('insert into url_error(urlMd5, url, rightUrlMd5, rightUrl, errorMessage, solveFlag, solution, insertTime, updateTime) values(%s,%s,%s,%s,%s,%s,%s,%s,%s)', (urlError.urlMd5, urlError.url, urlError.rightUrlMd5, urlError.rightUrl, urlError.errorMessage, urlError.solveFlag, urlError.solution, nowStr, nowStr))
+    conn.commit()
+    DBConnectionUtil.closeConn(conn , cur)
+
+
+
 #根据urlmd5置solveFlag标志位
 def updateUrlErrorByUrlMd5(urlMd5 , rightUrlMd5, rightUrl, solveFlag):
     nowStr = time.strftime('%Y-%m-%d %H:%M:%S')
